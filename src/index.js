13ea27c2515374dc566f33cbc8bb35fff84fbcdb
ShubhamRaj16 <Shubham.Sam.Raj@gmail.com>
2017-12-25 01:04:05 +0530

    no message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-const Koa = require('koa');
-const App = new Koa();
+
+ import Koa from 'koa';
+ import serv from './serv/';
+ // const Koa = require('koa');
+ const App = new Koa();
 
 App.use(async (ctx,next) =>{
     ctx.body ="step1";
---
+++ src/serv/index.js
@@ -0,0 +1,5 @@
+
+ async function serv(params) {
+         
+}
+export default serv; 
